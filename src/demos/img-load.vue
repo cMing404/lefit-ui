1c05ca8dcc2cc2b3bf869d7cc0aa7b5399e91f06
cMing <mayday404@163.com>
2017-06-08 11:38:04 +0800

    调整图片加载插件代码 增加单独元素可以调整大小

--- src/demos/img-load.vue
+++ src/demos/img-load.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div class="img-group-1">
-      <img v-lefit-load="'werwerwer'" alt="">
+      <img v-lefit-load="{url: 'werwerwer', scale: 0.5}" alt="">
+      <img v-lefit-load="'12323'" alt="">
     </div>
-    <div class="div" v-lefit-load="'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496828832817&di=e7c4693b42300027c779b6f12246b000&imgtype=0&src=http%3A%2F%2Fimage1.xyzs.com%2Fupload%2F3d%2Fc4%2F513%2F20150506%2F143087288726017_0.jpg'"></div>
+    <div class="div" v-lefit-load="{url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496828832817&di=e7c4693b42300027c779b6f12246b000&imgtype=0&src=http%3A%2F%2Fimage1.xyzs.com%2Fupload%2F3d%2Fc4%2F513%2F20150506%2F143087288726017_0.jpg', scale: 1}"></div>
   </div>
 </template>
 <script></script>
--- src/plugins/img-load/index.js
+++ src/plugins/img-load/index.js
@@ -6,58 +6,50 @@ let pluginObj = {
   options: {
     scale: 0.4
   },
-  lefitImgLoad (el) {
+  lefitImgLoad (el, scale) {
     el.style.backgroundColor = 'rgba(0,0,0,.03)'
     el.style.backgroundImage = `url(${loading})`
     el.style.backgroundPosition = 'center center'
     el.style.backgroundRepeat = 'no-repeat'
-    el.style.backgroundSize = `${189 * this.options.scale}px ${52 * this.options.scale}px`
+    el.style.backgroundSize = `${189 * (scale ? scale : this.options.scale)}px ${52 * (scale ? scale : this.options.scale)}px`
   },
-  lefitImgErr (el) {
+  lefitImgErr (el, scale) {
     el.style.backgroundColor = 'rgba(0,0,0,.03)'
     el.style.backgroundImage = `url(${err})`
     el.style.backgroundPosition = 'center center'
     el.style.backgroundRepeat = 'no-repeat'
-    el.style.backgroundSize = `${131 * this.options.scale}px ${125 * this.options.scale}px`
-  },
-  imgLoad (el, binding) {
-    let img = new Image()
-    img.src = binding.value
-    img.onload = function () {
-      if (el.nodeName === 'IMG') {
-        el.src = this.src
-      } else {
-        el.style.background = originStyle
-        el.style.backgroundImage = `url(${this.src})`
-      }
-      return
-    }
-    img.onerror = () => {
-      el.alt = `图片src请求失败: ${binding.value}`
-      this.lefitImgErr(el)
-    }
+    el.style.backgroundSize = `${131 * (scale ? scale : this.options.scale)}px ${125 * (scale ? scale : this.options.scale)}px`
   }
 }
 export default {
   install (Vue, scale = 0.4) {
     pluginObj.options.scale = scale
-    let H = window.screen.height
-    Vue.directive('lefit-load', {
-      bind (el) {
+    Vue.directive('lefit-load', function (el, binding) {
+      if (binding.value === undefined) {
+        return false
+      }
+      let valType = typeof binding.value === 'string' ? 'string' : 'object'
+      let originStyle = ''
+      if (el.nodeName !== 'IMG') {
+        originStyle = getComputedStyle(el).background
+      } else {
         el.src = placeImgSrc
-      },
-      inserted: (el, binding) => {
-        if (el.nodeName !== 'IMG') {
-          originStyle = getComputedStyle(el).background
+      }
+      pluginObj.lefitImgLoad(el, valType === 'object' ? binding.value.scale : undefined)
+      let img = new Image()
+      img.src = valType === 'string' ? binding.value : binding.value.url
+      img.onload = function () {
+        if (el.nodeName === 'IMG') {
+          el.src = this.src
+        } else {
+          el.style.background = originStyle
+          el.style.backgroundImage = `url(${this.src})`
         }
-        pluginObj.lefitImgLoad(el)
-        pluginObj.imgLoad(el, binding)
-      },
-      componentUpdated (el, binding) {
-        pluginObj.imgLoad(el, binding)
-      },
-      unbind (el) {
-        el.alt = ''
+        return
+      }
+      img.onerror = function () {
+        el.alt = `图片src请求失败: ${this.src}`
+        pluginObj.lefitImgErr(el, valType === 'object' ? binding.value.scale : undefined)
       }
     })
   }
